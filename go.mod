43fcd8faa4ca2189b1b077894769cf40573d5c6c
Anh Lai <laingocanh1990@gmail.com>
2022-09-18 22:47:58 +0700

    Writing Scalable Authentication in Golang Using Go-Guardian

---
+++ go.mod
@@ -0,0 +1,9 @@
+module scalable-auth
+
+go 1.18
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/shaj13/go-guardian v1.5.11 // indirect
+)
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/shaj13/go-guardian/auth"
+	"github.com/shaj13/go-guardian/auth/strategies/basic"
+	"github.com/shaj13/go-guardian/auth/strategies/bearer"
+	"github.com/shaj13/go-guardian/store"
+)
+
+var authenticator auth.Authenticator
+var cache store.Cache
+
+func main() {
+	port := os.Getenv("PORT")
+	setupGoGuardian()
+	router := mux.NewRouter()
+	router.HandleFunc("/v1/auth/token", middleware(http.HandlerFunc(createToken))).Methods("GET")
+	router.HandleFunc("/v1/book/{id}", middleware(http.HandlerFunc(getBookAuthor))).Methods("GET")
+	log.Printf("server started and listening on http://127.0.0.1:%s", port)
+	http.ListenAndServe("127.0.0.1:"+port, router)
+}
+
+func createToken(w http.ResponseWriter, r *http.Request) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iss": "auth-app",
+		"sub": "medium",
+		"aud": "any",
+		"exp": time.Now().Add(time.Minute * 5).Unix(),
+	})
+	jwtToken, _ := token.SignedString([]byte("secret"))
+	w.Write([]byte(jwtToken))
+}
+
+func getBookAuthor(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	books := map[string]string{
+		"1449311601": "Ryan Boyd",
+		"148425094X": "Yvonne Wilson",
+		"1484220498": "Prabath Siriwarden",
+	}
+	body := fmt.Sprintf("Author: %s \n", books[id])
+	w.Write([]byte(body))
+}
+
+func setupGoGuardian() {
+	authenticator = auth.New()
+	cache = store.NewFIFO(context.Background(), time.Minute*10)
+
+	basicStrategy := basic.New(validateUser, cache)
+	tokenStrategy := bearer.New(verifyToken, cache)
+
+	authenticator.EnableStrategy(basic.StrategyKey, basicStrategy)
+	authenticator.EnableStrategy(bearer.CachedStrategyKey, tokenStrategy)
+}
+
+func validateUser(ctx context.Context, r *http.Request, userName, password string) (auth.Info, error) {
+	// here connect to db or any other service to fetch user and validate it.
+	if userName == "medium" && password == "medium" {
+		return auth.NewDefaultUser("medium", "1", nil, nil), nil
+	}
+
+	return nil, fmt.Errorf("invalid credentials")
+}
+
+func middleware(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing Auth Middleware")
+		user, err := authenticator.Authenticate(r)
+		if err != nil {
+			code := http.StatusUnauthorized
+			http.Error(w, http.StatusText(code), code)
+			return
+		}
+		log.Printf("User %s Authenticated\n", user.UserName())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func verifyToken(ctx context.Context, r *http.Request, tokenString string) (auth.Info, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("secret"), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		user := auth.NewDefaultUser(claims["sub"].(string), "", nil, nil)
+		return user, nil
+	}
+
+	return nil, fmt.Errorf("invaled token")
+}
